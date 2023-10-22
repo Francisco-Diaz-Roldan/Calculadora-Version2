87693df794dd28a38c22df1a89420af508c3e067
Francisco_Diaz_Roldan <pacodiaz008@gmail.com>
2023-10-22 18:40:58 +0200

    Continúo con el JAVADOC de las Clases Main, Calculadora, BotonManager y Constantes finalizadas.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,12 @@ import org.example.ui.Calculadora;
 
 /**
  * Esta es la clase principal desde la que se inicializa la aplicación de la calculadora.
- * Crea una instancia de la calculadora y la muestra por pantalla
  */
 public class Main {
+    /**
+     * Este es el método principal que inicia la aplicación de la calculadora.
+     * @param args Los argumentos pasados a la aplicación (no se utilizan en este caso).
+     */
     public static void main(String[] args) {
             Calculadora calculadora = new Calculadora();
             calculadora.mostrar();
