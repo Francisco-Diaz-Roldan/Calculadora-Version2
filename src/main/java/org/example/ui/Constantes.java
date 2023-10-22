0d725abd493d55c86ed1edbe09f3ecc4208f305a
Francisco_Diaz_Roldan <pacodiaz008@gmail.com>
2023-10-22 18:35:52 +0200

    Continúo con el JAVADOC de las Clases Main, Calculadora, BotonManager y Constantes finalizadas.

--- src/main/java/org/example/ui/Constantes.java
+++ src/main/java/org/example/ui/Constantes.java
@@ -1,19 +1,53 @@
 package org.example.ui;
 
+/**
+ * Clase que define constantes utilizadas en una calculadora.
+ */
 public class Constantes {
-    // Defino las constantes para los mensajes de error y números del 1 al 9
-    // Defino la constante para el mensaje de error en caso de división por cero
+    //Defino las constantes con los mensajes de error
+    /**
+     * Mensaje de error en caso de división por cero.
+     */
     public static final String ERROR_DIVISION_CERO ="No se puede dividir por 0";
-    // Defino la constante para el mensaje de error en caso de intentar hacer la raíz de un número negativo
+    /**
+     * Mensaje de error en caso de intentar calcular la raíz de un número negativo.
+     */
     public static final String ERROR_RAIZ ="Imposible raiz negativa";
     // Defino las constantes para representar los números del 1 al 9
+    /**
+     * Representación constante del número 1.
+     */
     public static final String UNO = "1";
+    /**
+     * Representación constante del número 2.
+     */
     public static final String DOS = "2";
+    /**
+     * Representación constante del número 3.
+     */
     public static final String TRES = "3";
+    /**
+     * Representación constante del número 4.
+     */
     public static final String CUATRO = "4";
+    /**
+     * Representación constante del número 5.
+     */
     public static final String CINCO = "5";
+    /**
+     * Representación constante del número 6.
+     */
     public static final String SEIS = "6";
+    /**
+     * Representación constante del número 7.
+     */
     public static final String SIETE = "7";
+    /**
+     * Representación constante del número 8.
+     */
     public static final String OCHO = "8";
+    /**
+     * Representación constante del número 9.
+     */
     public static final String NUEVE = "9";
 }
